a689b302845011f65dae1cb71a1bc9c4e0816cfc
Victor <victor.maintenant@etud.univ-jfc.fr>
2021-01-15 17:39:12 +0100

    Fin Implémentation des tests pour la table Personne

--- src/main/java/galerie/dao/PersonneRepository.java
+++ src/main/java/galerie/dao/PersonneRepository.java
@@ -1,4 +1,5 @@
 package galerie.dao;
+
 import galerie.entity.Personne;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,10 +10,17 @@ import org.springframework.data.jpa.repository.Query;
  */
 public interface PersonneRepository extends JpaRepository<Personne, Integer> {
 
-    @Query(value = "SELECT SUM(prix_vente) AS budget FROM Personne " +
-        "INNER JOIN Transaction ON Transaction.personne_id = Personne.id "+
-        "WHERE extract(year FROM Transaction.vendu_le) = :annee and Personne.id = :id "+
-        "Group by Personne.nom",
-        nativeQuery = true)
-    float budgetArtSQL(int annee, int id);
+    @Query(value = "SELECT SUM(prix_vente) AS budget FROM Personne "
+            + "INNER JOIN Transaction ON Transaction.personne_id = Personne.id "
+            + "WHERE extract(year FROM Transaction.vendu_le) = :annee and Personne.id = :id "
+            + "Group by Personne.nom",
+            nativeQuery = true)
+    public Float budgetArtSQL(int annee, int id);
+
+    @Query(
+            value = "SELECT CASE WHEN EXISTS (SELECT id FROM Transaction "
+            + "Where Transaction.personne_id = :id) "
+            + "THEN CAST(1 as BIT) ELSE CAST(0 as BIT) END",
+            nativeQuery = true)
+    public Boolean aDesTransaction(Integer id);
 }
--- src/test/java/galerie/dao/PersonneRepositoryTest.java
+++ src/test/java/galerie/dao/PersonneRepositoryTest.java
@@ -19,6 +19,8 @@ import galerie.entity.Transaction;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
+import java.util.Optional;
 
 @Log4j2 // Génère le 'logger' pour afficher les messages de trace
 @DataJpaTest
@@ -28,7 +30,7 @@ public class PersonneRepositoryTest {
     private PersonneRepository personneDAO;
 
     @Test
-    @Sql("testPersonne-data.sql") // On peut charger des donnnées spécifiques pour un test
+    @Sql("test-data.sql") // On peut charger des donnnées spécifiques pour un test
     public void onSaitCompterLesEnregistrements() {
         log.info("On compte les enregistrements de la table 'Personne'");
         int combienDansLeJeuDeTest = 4; 
@@ -37,7 +39,7 @@ public class PersonneRepositoryTest {
     }
     
     @Test
-    @Sql("testCAannee-data.sql")
+    @Sql("test-data.sql")
     public void connaitreLeBudgetArtSQL(){
         log.info("On calcule le CA d'une galerie donnée selon une année donnée.");
         assertEquals(40000, personneDAO.budgetArtSQL(2020, 1), "On doit avoir 40000€ de recette");
@@ -56,5 +58,66 @@ public class PersonneRepositoryTest {
         assertFalse(p1.getTransactions().isEmpty());
         assertEquals(40000, p1.budgetArt(Date.valueOf(LocalDate.now()).getYear()));
 
+    }
+    
+    @Test
+    @Sql("test-data.sql")
+    void listerLesEntites() {
+        log.info("Lister les entités");
+        List<Personne> liste = personneDAO.findAll(); // Renvoie la liste des entités dans la table
+        log.info("Liste des entités: {}", liste);
+    }
+
+    @Test
+    @Sql("test-data.sql")
+    void touverParCle() {
+        log.info("Trouver une entité par sa clé");
+        int codePresent = 1;
+        Optional<Personne> resultat = personneDAO.findById(codePresent);
+        // On s'assure qu'on trouvé le résultat
+        assertTrue(resultat.isPresent(), "Cette galerie existe");
+        Personne p = resultat.get();
+        assertEquals("Bastide", p.getNom());
+        log.info("Entité trouvée: {}", p);
+    }
+
+    @Test
+    @Sql("test-data.sql")
+    void entiteInconnue() {
+        log.info("Chercher une entité inconnue");
+        int codeInconnu = 99;
+        Optional<Personne> resultat = personneDAO.findById(codeInconnu);
+        assertFalse(resultat.isPresent(), "Cette catégorie n'existe pas");
+
+    }
+
+    @Test
+    @Sql("test-data.sql")
+    void creerUneEntite() {
+        log.info("Créer une entité");
+        Personne nouvelle = new Personne();
+        nouvelle.setNom("Test");
+        nouvelle.setAdresse("Paris, France");
+        assertNull(nouvelle.getId(), "L'entité n'a pas encore de clé");
+        personneDAO.save(nouvelle); // 'save' enregistre l'entite dans la base
+        Integer nouvellecle = nouvelle.getId(); // La clé a été auto-générée lors de l'enregistrement
+        assertNotNull(nouvellecle, "Une nouvelle clé doit avoir été générée");
+        log.info("Nouvelle entité: {}", nouvelle);
+    }
+
+    @Test
+    @Sql("test-data.sql")
+    void onNePeutPasDetruireUnePersonneQuiADesTransaction() {
+        log.info("Détruire une catégorie avec des produits");
+        Personne p = personneDAO.getOne(1);
+        assertEquals("Bastide", p.getNom());
+        // Il y a des produits dans la catégorie 'Boissons'
+        assertTrue(personneDAO.aDesTransaction(p.getId()));
+        // Si on essaie de détruire cette catégorie, on doit avoir une exception
+        // de violation de contrainte d'intégrité
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            personneDAO.delete(p);
+            personneDAO.flush(); // Pour forcer la validation de la transaction
+        });
     }
 }
